c8a84acfecaf353ee65941aacd7545987782a326
Vitaly <1967.maslennikov@gmail.com>
2023-07-19 20:42:49 +0300

    Домашнее задание 7

---
+++ BasicCalculator.java
@@ -0,0 +1,18 @@
+public class BasicCalculator implements Calculator {
+    @Override
+    public ComplexNumber add(ComplexNumber a, ComplexNumber b) {
+        return a.add(b);
+    }
+
+    @Override
+    public ComplexNumber multiply(ComplexNumber a, ComplexNumber b) {
+        return a.multiply(b);
+    }
+
+    @Override
+    public ComplexNumber divide(ComplexNumber a, ComplexNumber b) {
+        return a.divide(b);
+    }
+}
+    
+
